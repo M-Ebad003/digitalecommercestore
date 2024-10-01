36b8edc710bb87588e608e55a84658097601303c
ebad <ebadmuhammad003@gmail.com>
2024-10-01 23:13:20 +0500

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`relative h-full ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
       </body>
     </html>
   );
